ba9f1c8fefd45246f89c27379fc44abfacba55a5
kokosko <kostya.kokosko@gmail.com>
2015-04-14 15:13:31 +0300

    change database sqlite -> postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150409091942) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
     t.boolean  "done"
